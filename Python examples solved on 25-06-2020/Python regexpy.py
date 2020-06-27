70b2eb5fdadf0384c8b2ddbd0af90229e04cca4f
jyotiprasad10 <59274636+jyotiprasad10@users.noreply.github.com>
2020-06-27 20:04:40 +0530

    Add files via upload

---
+++ Python examples solved on 25-06-2020/Python regexpy.py
@@ -0,0 +1,15 @@
+import re
+t = int(input())
+for i in range(t):
+    try :
+        s = input()
+        re.compile(s)
+    except:
+        print("False")
+        continue
+    print("True")
+
+
+
+
+
